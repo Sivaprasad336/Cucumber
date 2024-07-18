a3f675a4fa01744b1e618137a2f3fd0394644e6c
Sivaprasad336 <pagadalasivaprasad1@gmail.com>
2024-07-18 10:16:50 +0530

    RegistrationPage Functionality

---
+++ eclipse-workspace/CucumberProject/CucumberDemo/src/main/java/com/tutorialsninja/pages/RegistrationPage.java
@@ -0,0 +1,10 @@
+package com.tutorialsninja.pages;
+
+public class RegistrationPage {
+
+	public static void main(String[] args) {
+		System.out.println("Hai welcome to the registration page");
+
+	}
+
+}
